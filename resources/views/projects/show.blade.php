c4897f26dc7279bd75a04cfc9dfb336cb1e2682c
Emanuel Rybak <ery@pc-tec.local>
2019-05-03 13:48:09 +0200

    modified color scheme

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,11 +4,21 @@
 
 <div class="container">
 	<b-jumbotron>
-		<template slot="header">{{$project->title}}</template>
-		<template slot="lead">Created: {{$project->created_at}}</template>
+		<template slot="header">
+			{{ $project->title }}
+			<hr class="my-4">
+		</template>
+
+		<template slot="lead">Created: {{ $project->created_at }}</template>
 	</b-jumbotron>
 	<b-form inline method="GET" action="{{ route('projects.edit', $project->id) }}">
 		<b-button type="submit" variant="primary">Edit project</b-button>
 	</b-form>
+</div>
+<br>
+<div class="container">
+	<h1>{{ $project->title }}'s tasks</h1>
+<b-table striped hover :items="{{ $project->tasks }}">
+		</b-table>
 </div>
 @endsection
