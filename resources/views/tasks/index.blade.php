9e7d53fc952a4dafc4d3168249fd9dc7ccc8f5dd
Emanuel Rybak <ery@pc-tec.local>
2019-05-06 12:11:05 +0200

    Removed unnecessary tasks blade views

--- resources/views/tasks/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<tasks-index tasks="{{ $tasks }}"></tasks-index>
-
-@endsection
--- resources/views/tasks/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<tasks-show task="{{ $task }}"></tasks-show>
-
-@endsection
